a4e9b42114dafa2d5899754fe50a7da6092b6d9d
nourhanahmedelbadawie <nourhanahmedelbadawie@gmail.com>
2026-05-19 08:21:24 +0300

    AAA Pattern for testing

--- Frontend/math.test.js
+++ Frontend/math.test.js
@@ -2,6 +2,11 @@ import { add } from "./math";
 import {  it, expect } from "vitest";
 
 it("should add two numbers", () => {
-    const result = add(1, 2);
+    //==> AAA Pattern for testing
+    // Arrange  
+    const numbers = [1, 2, 3, 4, 5];
+    // Act
+    const result = add(numbers);
+    // Assert
     expect(result).toBe(3);
 });
